730f0bcf0ddff4c0daf664e398e4ecc1326054d5
Ibraim69 <99362220+Ibraim69@users.noreply.github.com>
2022-09-26 09:36:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tipove_danni_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedi bukva");
+            char bukva = 'c';
+            byte chislo = 100;
+            short chislo2 = 20000;
+            int chislo3 = 300000;
+            string duma = "Programirane";
+            Console.WriteLine("bukvata e = " +  bukva);
+            Console.WriteLine("chislo1  e = " + chislo);
+            Console.WriteLine("chislpo2 e = " + chislo2);
+            Console.WriteLine("chislo2 e = " + chislo3);
+            Console.WriteLine("dumata  e = "  + duma);
+        }
+    }
+}
